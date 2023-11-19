f3f1179b03fd762bac2f64a4b9a372f14647bb1f
riya7701 <you@example.com>
2023-11-20 01:14:39 +0530

    Updates

---
+++ precache-manifest.bc1260e899a36093f7a92213fe6e6ad0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d686aa63dcf7da99640e5635d5a522d",
+    "url": "/Dashtoon_task/index.html"
+  },
+  {
+    "revision": "318dea6e974d8dfb14b4",
+    "url": "/Dashtoon_task/static/css/main.cd73aa3f.chunk.css"
+  },
+  {
+    "revision": "1bb5227adebc85c38cf9",
+    "url": "/Dashtoon_task/static/js/2.a35e277c.chunk.js"
+  },
+  {
+    "revision": "be54990eac6d3dd1242e18aade05a154",
+    "url": "/Dashtoon_task/static/js/2.a35e277c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896e0cb3dd0fe37a2210",
+    "url": "/Dashtoon_task/static/js/3.e2023eda.chunk.js"
+  },
+  {
+    "revision": "318dea6e974d8dfb14b4",
+    "url": "/Dashtoon_task/static/js/main.fdf99cb9.chunk.js"
+  },
+  {
+    "revision": "71209794cfdb43ed2a8f",
+    "url": "/Dashtoon_task/static/js/runtime-main.29f591fd.js"
+  },
+  {
+    "revision": "de8c7b364a2e9a370224e008791588a2",
+    "url": "/Dashtoon_task/static/media/bg.de8c7b36.jpg"
+  }
+]);
